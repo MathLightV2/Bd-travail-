322285ebc663af5c6ed0f7b0a72b6267642b3ab4
romain <romain.fuocobinette@gmail.com>
2023-05-02 11:34:26 -0400

    Update creation_outils.sql

--- creation_outils.sql
+++ creation_outils.sql
@@ -1,4 +1,18 @@
 -- ROMAIN
+CREATE OR REPLACE PROCEDURE ajout_employe_departement(
+	nom				employe.nom%TYPE
+	,prenom			employe.prenom%TYPE
+	,genre			employe.genre%TYPE
+	,nas			employe.nas%TYPE
+	,salaire		employe.salaire%TYPE
+	,date			employe.date_emabauche%TYPE
+	,poste			employe.poste%TYPE
+	,departement	employe.departement%TYPE
+	LANGUAGE SQL
+	AS $$			
+	INSERT INTO employe(nom, prenom, genre, nas, salaire, date_emabauche, poste, departement)
+			VALUES (nom,prenom ,genre ,nas ,salaire ,date ,poste ,departement);		
+	$$;
 
 -- NOÉ
 
