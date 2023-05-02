74531539438cee8568b425421915fff1376af9c2
bibz43 <68484992+Juliettevincent@users.noreply.github.com>
2023-05-02 08:24:00 -0400

    insertion troncon

--- insertions.sql
+++ insertions.sql
@@ -15,7 +15,18 @@ VALUES
 (nextval('seq_tron_id'), 2, 3, 11, 1200, 50, 2, "asphalte"),--5
 (nextval('seq_tron_id'), 3, 5, 6, 217.78, 50, 2, "asphalte"),--6
 (nextval('seq_tron_id'), 3, 6, 7, 237.51, 50, 2, "asphalte"),--7
-(nextval('seq_tron_id')),--8
-(nextval('seq_tron_id')), -- 9
-(nextval('seq_tron_id')), -- 10
-(nextval('seq_tron_id')), -- 11
+(nextval('seq_tron_id'), 3, 7, 8, 246.30, 50, 2, "asphalte"),--8
+(nextval('seq_tron_id'), 3, 8, 9, 236.38, 50, 2, "beton"), -- 9
+(nextval('seq_tron_id'), 3, 9, 10, 321.75, 50, 2, "beton"), -- 10
+(nextval('seq_tron_id'), 3, 10, 11, 361.68, 50, 2, "asphalte"), -- 11
+(nextval('seq_tron_id'), 3, 11, 1, 230.10, 50, 2, "asphalte"), -- 12
+(nextval('seq_tron_id'), 1, 3, 2, 717.69, 50, 2, "asphalte"), -- 13
+(nextval('seq_tron_id'), 5, 2, 1, 1070, 50, 2, "asphalte"), -- 14
+(nextval('seq_tron_id'), 1, 4, 3, 1000, 30, 2, "asphalte"), -- 15
+(nextval('seq_tron_id'), 4, 5, 4, 1100, 50, 2, "asphalte"), -- 16
+(nextval('seq_tron_id'), 2, 11, 3, 1200, 50, 2, "asphalte"), -- 17
+(nextval('seq_tron_id'),  50, 2, "asphalte"), -- 18
+(nextval('seq_tron_id'), 3, 3, 10, 11, 361.68, 50, 2, "asphalte"), -- 20
+(nextval('seq_tron_id'), 3, 3, 10, 11, 361.68, 50, 2, "asphalte"), -- 21
+(nextval('seq_tron_id'), 3, 3, 10, 11, 361.68, 50, 2, "asphalte"), -- 22
+(nextval('seq_tron_id'), 3, 3, 10, 11, 361.68, 50, 2, "asphalte"), -- 24
