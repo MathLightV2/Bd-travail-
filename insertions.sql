9f6ff60794551f965570f92f9cd78db1377920f6
bibz43 <68484992+Juliettevincent@users.noreply.github.com>
2023-05-01 23:10:11 -0400

    update outils & insertions -ju

--- insertions.sql
+++ insertions.sql
@@ -5,3 +5,17 @@
 -- MATHIS
 
 --JULIETTE
+-- table troncon
+INSERT INTO troncon
+VALUES
+(nextval('seq_tron_id'), 1, 2, 3, 717.69, 50, 2, "asphalte"), --1
+(nextval('seq_tron_id'), 5, 1, 2, 1070, 50, 2,"asphalte"), --2
+(nextval('seq_tron_id'), 1, 3, 4, 1000, 30, 2, "asphalte"),--3
+(nextval('seq_tron_id'), 4, 4, 5, 1100, 50, 2, "asphalte"),--4
+(nextval('seq_tron_id'), 2, 3, 11, 1200, 50, 2, "asphalte"),--5
+(nextval('seq_tron_id'), 3, 5, 6, 217.78, 50, 2, "asphalte"),--6
+(nextval('seq_tron_id'), 3, 6, 7, 237.51, 50, 2, "asphalte"),--7
+(nextval('seq_tron_id')),--8
+(nextval('seq_tron_id')), -- 9
+(nextval('seq_tron_id')), -- 10
+(nextval('seq_tron_id')), -- 11
