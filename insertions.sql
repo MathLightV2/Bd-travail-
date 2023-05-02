50d54d52f9fe2ef016f8fc1d18300348377c4c8d
MathLightV2 <98351755+MathLightV2@users.noreply.github.com>
2023-05-02 11:29:59 -0400

    Update insertions.sql

--- insertions.sql
+++ insertions.sql
@@ -4,4 +4,34 @@
 
 -- MATHIS
 
+-- insertion table type_pan MATHIS
+
+INSERT INTO type_pan (nom)
+VALUES 	('arrêt'),
+		('céder'),
+		('ralentir'),
+		('limite_de_vitesse');
+		
+		
+-- insertion table type_dis_par MATHIS
+
+INSERT INTO type_dis_par(nom)
+VALUES 	('accès_fauteuil'),
+		('signal_audio_piéton'),
+		('radar');
+		
+
+-- insertion forme MATHIS 
+
+INSERT INTO forme(nom)
+VALUES 	('ronde'),
+		('carrée'),
+		('losange'),
+		('flèche'),
+		('humains'),
+		('main'),
+		('vélo'),
+		('barre_verticale'),
+		('barre horizontale');
+
 --JULIETTE
