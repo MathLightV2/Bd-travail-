c610001b3489b53a20d83b9717a123017904531f
romain <romain.fuocobinette@gmail.com>
2023-05-03 13:46:00 -0400

    insertion, procedures, fonctions

--- creation_outils.sql
+++ creation_outils.sql
@@ -1,19 +1,82 @@
 -- ROMAIN
-CREATE OR REPLACE PROCEDURE ajout_employe_departement(
-	nom				employe.nom%TYPE
-	,prenom			employe.prenom%TYPE
-	,genre			employe.genre%TYPE
-	,nas			employe.nas%TYPE
-	,salaire		employe.salaire%TYPE
-	,date			employe.date_emabauche%TYPE
-	,poste			employe.poste%TYPE
-	,departement	employe.departement%TYPE
+DROP FUNCTION IF EXISTS id_type_pan, id_dis_pan, id_poste, id_departement, id_employe;
+
+CREATE OR REPLACE FUNCTION id_type_pan(
+	_nom type_pan.nom%TYPE)
+	RETURNS INT
 	LANGUAGE SQL
-	AS $$			
-	INSERT INTO employe(nom, prenom, genre, nas, salaire, date_emabauche, poste, departement)
-			VALUES (nom,prenom ,genre ,nas ,salaire ,date ,poste ,departement);		
+	AS $$
+		SELECT id_type_pan FROM type_pan WHERE _nom = nom
+	$$;
+	
+CREATE OR REPLACE FUNCTION id_dis_pan(
+	_nom type_dis_par.nom%TYPE)
+	RETURNS INT
+	LANGUAGE SQL
+	AS $$
+		SELECT id_type_dis_par FROM type_dis_par WHERE _nom = nom
+	$$;
+	
+CREATE OR REPLACE FUNCTION id_poste(
+	_titre poste.titre%TYPE)
+	RETURNS INT
+	LANGUAGE SQL
+	AS $$
+		SELECT id_poste FROM poste WHERE _titre = titre
+	$$;
+	
+CREATE OR REPLACE FUNCTION id_departement(
+	_nom departement.nom%TYPE)
+	RETURNS INT
+	LANGUAGE SQL
+	AS $$
+		SELECT id_departement FROM departement WHERE _nom = nom
 	$$;
 
+CREATE OR REPLACE FUNCTION id_employe(
+	_nom employe.nom%TYPE
+	,_prenom employe.prenom%TYPE)
+	RETURNS INT
+	LANGUAGE SQL
+	AS $$
+		SELECT id_employe FROM employe WHERE (_nom = nom) AND (_prenom = prenom)
+	$$;
+
+CREATE PROCEDURE ajout_employe(
+	nom				employe.nom%TYPE
+	,prenom			employe.prenom%TYPE
+	,genre			CHAR(1)
+	,nas			employe.nas%TYPE
+	,salaire		employe.salaire%TYPE
+	,date			employe.date_embauche%TYPE
+	,poste			poste.titre%TYPE
+	,departement	departement.nom%TYPE)
+	LANGUAGE SQL
+	AS $$			
+	INSERT INTO employe(nom, prenom, genre, nas, salaire, date_embauche, poste, departement)
+			VALUES (nom, prenom , CAST(UPPER(genre) AS GENRE),  nas ,salaire ,date , id_poste(poste) , id_departement(departement));
+	$$;
+	
+CREATE PROCEDURE ajout_signalisation(
+	_orientation			signalisation.orientation%TYPE
+	,_position				signalisation.position%TYPE
+	,_troncon				signalisation.troncon%TYPE)
+	LANGUAGE SQL
+	AS $$			
+	INSERT INTO signalisation (orientation,"position",troncon)
+			VALUES (_orientation, _position, _troncon);
+	$$;	
+	
+CREATE PROCEDURE ajout_panneau(
+	_type					type_pan.nom%TYPE
+	,_position				panneau.position%TYPE
+	,_troncon				panneau.troncon%TYPE)
+	LANGUAGE SQL
+	AS $$			
+	INSERT INTO panneau ("type", "position", troncon)
+			VALUES (id_type_pan(_type), _position, _troncon);
+	$$;		
+	
 -- NOÉ
 
 -- MATHIS
--- insertions.sql
+++ insertions.sql
@@ -1,4 +1,30 @@
 -- ROMAIN
+INSERT INTO poste (titre)
+	VALUES 	('professionnel'), ('technicien'), ('ingénieur'), ('scientifique'), ('manutentionnaire'), ('soutient');
+
+INSERT INTO departement (nom)
+	VALUES 	('administration'), ('ventes et représentation'), ('achats'), ('mécanique'), ('électrique'), ('informatique'), ('recherche');
+
+CALL ajout_employe('Fuoco-Binette', 'Romain', 'h', '123456789', 35.30, '2018-01-01', 'ingénieur', 'mécanique');
+CALL ajout_employe('Leclerc', 'Mathis', 'h', '987654321', 15.00, '2020-02-01', 'manutentionnaire', 'informatique');
+CALL ajout_employe('Bousquet', 'Noé', 'h', '123459876', 27.50, '2021-03-01', 'technicien', 'électrique');
+CALL ajout_employe('Vincent', 'Juliette', 'f', '987651234', 150.75, '2023-04-01','scientifique', 'recherche');					   
+CALL ajout_employe('Webster', 'Patrick', 'H', '654321789', 220.3, '2023-04-01', 'professionnel', 'informatique');
+
+CALL ajout_signalisation('verticale', 100, 2);
+CALL ajout_signalisation('horizontale', 100, 8);
+CALL ajout_signalisation('verticale', 100, 9);
+CALL ajout_signalisation('verticale', 75, 5);
+CALL ajout_signalisation('verticale', 10, 11);
+CALL ajout_signalisation('verticale', 50, 22);
+CALL ajout_signalisation('verticale', 100, 21);
+CALL ajout_signalisation('verticale', 88, 20);
+
+CALL ajout_panneau('arret', 100, 10);
+CALL ajout_panneau('limite_de_vitesse', 50, 10);
+CALL ajout_panneau('arret', 100, 5);
+CALL ajout_panneau('ceder', 100, 12);
+CALL ajout_panneau('ralentir', 60, 4);
 
 -- NOÉ
 
