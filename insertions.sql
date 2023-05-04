3a572584d0c65636c627af4f26ce8de4bdba9dad
MathLightV2 <98351755+MathLightV2@users.noreply.github.com>
2023-05-04 19:37:14 -0400

    ajout des profileur et date plus réel
    manque a ajouter les employé

--- insertions.sql
+++ insertions.sql
@@ -43,14 +43,16 @@ VALUES 	('Solinst','485321','2008-09-14','2018-10-19'),
 		
 -- insertion calibration MATHIS
 INSERT INTO calibration (id_calibration,date_debut,date_fin,employe,v1,v2,v3,profileur)
-VALUES 	(nextval('seq_cali_id'),'2018-11-24 10:10:53','2019-11-24 10:10:53','',250.03,250.07,250.08,(SELECT id)),
-		(nextval('seq_cali_id'),'2020-11-24 10:10:53','2019-11-24 10:10:53','',290.03,290.07,290.08,(SELECT id)),
-		(nextval('seq_cali_id'),'2021-11-24 10:10:53','2019-11-24 10:10:53','',301.03,302.07,304.08,(SELECT id)),
-		(nextval('seq_cali_id'),'2015-11-24 10:10:53','2019-11-24 10:10:53','',260.03,250.07,250.08,(SELECT id)),
-		(nextval('seq_cali_id'),'2016-11-24 10:10:53','2019-11-24 10:10:53','',265.03,250.07,250.08,(SELECT id)),
-		(nextval('seq_cali_id'),'2017-11-24 10:10:53','2019-11-24 10:10:53','',280.03,250.07,250.08,(SELECT id)),
-		(nextval('seq_cali_id'),'2018-11-24 10:10:53','2019-11-24 10:10:53','',270.03,250.07,250.08,(SELECT id)),
-		(nextval('seq_cali_id'),'2019-11-24 10:10:53','2019-11-24 10:10:53','',150.05,150.05,150.05,(SELECT id));
+VALUES 	(nextval('seq_cali_id'),'2018-11-24 10:10:53','2019-11-24 10:10:53','TODO',250.03,250.07,250.08,(SELECT id)),
+		(nextval('seq_cali_id'),'2020-11-24 10:10:53','2010-11-24 10:10:53','TODO',290.03,290.07,290.08,(SELECT id_profileur FROM profileur WHERE marque ='Solinst')),
+		(nextval('seq_cali_id'),'2021-11-24 10:10:53','2021-11-24 10:10:53','TODO',301.03,302.07,304.08,(SELECT id_profileur FROM profileur WHERE marque ='Dektak' )),
+		(nextval('seq_cali_id'),'2015-11-24 10:10:53','2015-11-24 10:10:53','TODO',260.03,250.07,250.08,(SELECT id_profileur FROM profileur WHERE marque ='Dektak')),
+		(nextval('seq_cali_id'),'2016-11-24 10:10:53','2016-11-24 10:10:53','TODO',265.03,250.07,250.08,(SELECT id_profileur FROM profileur WHERE marque ='Dektak')),
+		(nextval('seq_cali_id'),'2017-11-24 10:10:53','2017-11-24 10:11:53','TODO',280.03,250.07,250.08,(SELECT id_profileur FROM profileur WHERE marque ='Normandin')),
+		(nextval('seq_cali_id'),'2018-11-24 10:10:53','2018-11-24 10:08:53','TODO',270.03,250.07,250.08,(SELECT id_profileur FROM profileur WHERE marque ='Normandin')),
+		(nextval('seq_cali_id'),'2021-11-24 10:10:53','2021-11-24 10:08:53','TODO',234.03,250.07,250.08,(SELECT id_profileur FROM profileur WHERE marque ='Normandin')),
+		(nextval('seq_cali_id'),'2018-11-24 10:10:53','2018-11-24 10:08:53','TODO',245.03,250.07,250.08,(SELECT id_profileur FROM profileur WHERE marque ='Normandin')),
+		(nextval('seq_cali_id'),'2023-11-24 10:10:53','2023-11-24 10:11:53','TODO',150.05,150.05,150.05,(SELECT id_profileur FROM profileur WHERE marque ='Normandin'));
 		
 
 --JULIETTE
