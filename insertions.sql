59c6bffc173c375aef1e6c54b30b16a63dc2ee75
bibz43 <68484992+Juliettevincent@users.noreply.github.com>
2023-05-12 16:38:17 -0400

    update insertion

--- insertions.sql
+++ insertions.sql
@@ -21,6 +21,52 @@ CREATE PROCEDURE ajout_dis_par(
         VALUES(_type, _position, _troncon)
 $$;
 
+CREATE OR REPLACE PROCEDURE insert_rand_lumiere()
+LANGUAGE PLPGSQL
+AS $$
+    DECLARE
+     _forme			lumiere.forme%TYPE;
+    _couleur			lumiere.couleur%TYPE;
+    _mode		    lumiere.mode%TYPE;
+    _signalisation	lumiere.signalisation%TYPE;
+
+    BEGIN
+
+            FOR i IN 1..10 LOOP
+            SELECT select_rand_id('id_forme', 'forme') INTO _forme;
+            SELECT select_rand_id('id_couleur', 'couleur') INTO _couleur;
+            SELECT * FROM unnest(enum_range(NULL::mode)) ORDER BY random() LIMIT 1 INTO _mode;
+            SELECT select_rand_id('id_signalisation', 'signalisation') INTO _signalisation;
+
+            INSERT INTO lumiere(forme, couleur, mode, signalisation)
+                VALUES (_forme, _couleur, _mode, _signalisation);
+                END LOOP;
+        END;
+$$;
+
+
+CREATE OR REPLACE PROCEDURE insert_rand_dis_particulier()
+LANGUAGE PLPGSQL
+AS $$
+    DECLARE 
+    _type           dis_particulier.type%TYPE;
+    _position       dis_particulier.position%TYPE;
+    _troncon        dis_particulier.troncon%TYPE;
+
+    BEGIN
+
+            FOR i IN 1..10 LOOP
+            SELECT select_rand_id('id_type_dis_par', 'type_dis_par') INTO _type;
+            SELECT (RANDOM()*100) INTO _position;
+            SELECT select_rand_id('id_troncon', 'troncon') INTO _troncon;
+
+            INSERT INTO dis_particulier(type, position, troncon)
+                    VALUES(_type, _position, _troncon);
+                    END LOOP;
+        END;
+$$;
+
+
 INSERT INTO rue(nom)
 VALUES
     ('Viau'),
@@ -65,10 +111,9 @@ VALUES
     ('orange', 'FFBF00'),
     ('blanc', 'FFFFFF');
 
-INSERT INTO dis_particulier(type, position, troncon)
-VALUES
+
  CALL ajout_dis_par(1, 100, 2);
- CALL ajout_dis_par(2, 0, 5),
+ CALL ajout_dis_par(2, 0, 5);
  CALL ajout_dis_par(3, 50, 7);
  CALL insert_rand_dis_particulier();
 
@@ -76,60 +121,15 @@ VALUES
 CALL ajout_lumiere(1, 1, 'solide', 1);
 CALL ajout_lumiere(1, 2, 'solide', 1);
 CALL ajout_lumiere(1, 3, 'solide', 1);
-CALL ajout_lumiere(2, 1, 'controle', 2);
-CALL ajout_lumiere(2, 2, 'controle', 2);
+CALL ajout_lumiere(2, 1, 'contrôlé', 2);
+CALL ajout_lumiere(2, 2, 'contrôlé', 2);
 CALL ajout_lumiere(3, 3, 'intelligente', 2);
 CALL ajout_lumiere(4, 1, 'solide', 3);
 CALL ajout_lumiere(4, 2, 'solide', 3);
 CALL ajout_lumiere(4, 3, 'solide', 3);
 CALL ajout_lumiere(6, 4, 'clignotant', 4);
-CALL ajout_lumiere(5, 5, 'controle', 5);
+CALL ajout_lumiere(5, 5, 'contrôlé', 5);
 CALL ajout_lumiere(7, 5, 'solide', 6);
 CALL ajout_lumiere(8, 3, 'solide', 7);
 CALL ajout_lumiere(9, 1, 'solide', 8);
 CALL insert_rand_lumiere();
-				
-CREATE OR REPLACE PROCEDURE insert_rand_lumiere()
-LANGUAGE PLPSQL
-AS $$
-    DECLARE
-     _forme			lumiere.forme%TYPE,
-    _couleur			lumiere.couleur%TYPE,
-    _mode		    lumiere.mode%TYPE,
-    _signalisation	lumiere.signalisation%TYPE
-
-    BEGIN
-
-            FOR i IN 1..10 LOOP
-            SELECT select_rand_id('id_forme', 'forme') INTO _forme;
-            SELECT select_rand_id('id_couleur', 'couleur') INTO _couleur;
-            SELECT * FROM unnest(enum_range(NULL::mode)) ORDER BY random() LIMIT 1 INTO _mode;
-            SELECT select_rand_id('id_signalisation', 'signalisation') INTO _signalisation;
-
-            INSERT INTO lumiere(forme, couleur, mode, signalisation)
-                VALUES (_forme, _couleur, _mode, _signalisation);
-                END LOOP;
-        END;
-$$;
-
-
-CREATE OR REPLACE PROCEDURE insert_rand_dis_particulier()
-LANGUAGE PLPSQL
-AS $$
-    DECLARE 
-    _type           dis_particulier.type%TYPE,
-    _position       dis_particulier.position%TYPE,
-    _troncon        dis_particulier.troncon%TYPE
-
-    BEGIN
-
-            FOR i IN 1..10 LOOP
-            SELECT select_rand_id('id_type_dis_par', 'type_dis_par') INFOR _type;
-            SELECT (RANDOM()*100) INTO _position;
-            SELECT select_rand_id('id_troncon', 'troncon') INTO _troncon;
-
-            INSERT INTO dis_particulier(type, position, troncon)
-                    VALUES(_type, _position, _troncon);
-                    END LOOP;
-        END;
-$$;
