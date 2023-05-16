28cc113a093d22a3ada48ea4862a6ebfb0a9ea6b
MathLightV2 <98351755+MathLightV2@users.noreply.github.com>
2023-05-16 19:34:45 -0400

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -18,4 +18,13 @@ votre requête
 
 -- MATHIS
 
+-- requête 1 mathis 
+
+SELECT emp.nom, 
+COUNT(*) AS nombre_calibrations
+FROM calibration AS cali
+	INNER JOIN employe AS emp 
+		ON cali.employe = emp.id_employe
+GROUP BY emp.nom;
+
 --JULIETTE
