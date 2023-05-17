7bc3096c20944366951c6b36a67d2b15faaf4720
MathLightV2 <98351755+MathLightV2@users.noreply.github.com>
2023-05-17 19:13:24 -0400

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -18,34 +18,60 @@ votre requête
 
 -- MATHIS
 
--- requête 1 mathis 
-
+-- =======================================================
+-- Requête #1
+-- Objectif : Donner le nombre de calibrations que chaque employé a fait.
+--
+--
+-- Évaluation : Fonctionnel
+-- Réalisé par : Mathis
+-- Aidé par :
+-- =======================================================
 SELECT emp.nom, 
 COUNT(*) AS nombre_calibrations
 FROM calibration AS cali
 	INNER JOIN employe AS emp 
 		ON cali.employe = emp.id_employe
 GROUP BY emp.nom;
+-- =======================================================
 
--- requête 2 mathis 
+-- =======================================================
+-- Requête #2
+-- Objectif : Pour chaque véhicule, combien de kilomètres de tronçons ont été parcourus pour réaliser
+les inspections.
+--
+--
+-- Évaluation : Fonctionnel
+-- Réalisé par : Mathis
+-- Aidé par :
+-- =======================================================
 SELECT v.immatriculation, 
 CONCAT(SUM(ins.km_fin_inspect - ins.km_debut_inspect), ' km')  AS kilometres_parcourus
 FROM inspection ins
 	INNER JOIN vehicule AS v
 		ON ins.vehicule = v.id_vehicule
 GROUP BY v.immatriculation;
+-- =======================================================
 
--- requête 3 mathis 
-
+-- =======================================================
+-- Requête #3
+-- Objectif : Retourne le nombre en ordre décroisant  des différentes 
+-- 		combinaisons de forme et de couleur se situant sur les
+--		différent tronçons
+-- Évaluation : Fonctionnel
+-- Réalisé par : Mathis
+-- Aidé par :
+-- =======================================================
 SELECT form.nom AS nom_forme, cou.nom AS nom_couleur, COUNT(*) AS nombre_fois
-FROM lumiere AS lum
-INNER JOIN signalisation AS sig 
-ON lum.signalisation = sig.id_signalisation
-INNER JOIN forme form 
-ON lum.forme = form.id_forme
-INNER JOIN couleur AS  cou 
-ON lum.couleur = cou.id_couleur
+	FROM lumiere AS lum
+	INNER JOIN signalisation AS sig 
+		ON lum.signalisation = sig.id_signalisation
+	INNER JOIN forme form 
+		ON lum.forme = form.id_forme
+	INNER JOIN couleur AS  cou 
+		ON lum.couleur = cou.id_couleur
 GROUP BY lum.forme, lum.couleur, form.nom, cou.nom
 ORDER BY nombre_fois DESC;
+-- =======================================================
 
 --JULIETTE
